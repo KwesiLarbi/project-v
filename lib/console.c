03f55b51ac3e406aebde184108eecdc0cb1215c3
KwesiLarbi <KwesiiiLarb@gmail.com>
2025-02-12 22:36:03 +0000

    improving i/o control with serial devices

--- lib/console.c
+++ lib/console.c
@@ -15,7 +15,78 @@ static void delay(void)
     inb(0x84);
 }
 
-/* Serial I/O code */
+/* Serial I/O code: used to control and communicate with serial devices */
+#define COM1        0x3F8
+
+#define COM_RX      0    /* In: Receive buffer (DLAB=0) */
+#define COM_TX      0    /* Out: Transmit buffer (DLAB=0) */
+#define COM_DLL     0   /* Out: Divisor Latch Low (DLAB=1) */
+#define COM_DLM     1   /* Out: Divisor Latch High (DLAB=1) */
+#define COM_IER     1   /* Out: Interrupt Enable Register */
+#define     COM_IER_RDI     0x01    /* Enable receiver data interrupt */
+#define COM_IIR     2   /* In: Interrupt ID Register */
+#define COM_FCR     2   /* Out: FIFO Control Register */
+#define COM_LCR     3   /* Out: Line Control Register */
+#define     COM_LCR_DLAB    0x80    /* Divisor Latch Access Bit */
+#define     COM_LCR_WLEN8   0x03    /* WordLength: 8 bits */
+#define COM_MCR     4  /* Out: Modem Control Register */
+#define     COM_MCR_RTS     0x02    /* RTS complement */
+#define     COM_MCR_DTR     0x01    /* DTR complement */
+#define     COM_MCR_OUT2    0x08    /* Out2 complement */
+#define COM_LSR     5   /* In: Line Status Register */
+#define     COM_LSR_DATA    0x01    /* Data available */
+#define     COM_LSR_TXRDY   0x20    /* Transmit buffer available */
+#define     COM_LSR_TSRE    0x40    /* Transmitter off */
+
+static bool serial_exists;
+
+static int serial_proc_data(void)
+{
+    if (!(inb(COM1 + COM_LSR) & COM_LSR_DATA))
+        return -1;
+    return inb(COM1 + COM_RX);
+}
+
+void serial_intr(void)
+{
+    if (serial_exists)
+        cons_intr(serial_proc_data);
+}
+
+static void serial_putc(int c)
+{
+    int i;
+    for (i = 0; !(inb(COM1 + COM_LSR) & COM_LSR_TXRDY) && i < 12800; i++)
+        delay();
+    
+    outb(COM1 + COM_TX, c);
+}
+
+static void serial_init(void)
+{
+    /* turn off the FIFO */
+    outb(COM1 + COM_FCR, 0);
+
+    /* set speed, requires DLAB latch */
+    outb(COM1 + COM_LCR, COM_LCR_DLAB);
+    outb(COM1 + COM_DLL, (uint8_t) (115200 / 9600));
+    outb(COM1 + COM_DLM);
+
+    /* 8 data bits, 1 stop bit, parity off; turn off DLAB latch */
+    outb(COM1 + COM_LCR, COM_LCR_WLEN8 & ~COM_LCR_DLAB);
+
+    /* no modem controls */
+    outb(COM1 + COM_MCR, 0);
+
+    /* enable rcv interrupts */
+    outb(COM1 + COM_IER, COM_IER_RDI);
+
+    /* clear any pre-existing overrun indications and interrupts */
+    /* serial port doesn't exist if COM_LSR returns 0xFF */
+    serial_exists = (inb(COM1 + COM_LSR) != 0xFF);
+    (void) inb(COM1 + COM_IIR);
+    (void) inb(COM1 + COM_RX);
+}
 
 /* Keyboard input code */
 #define NO          0
